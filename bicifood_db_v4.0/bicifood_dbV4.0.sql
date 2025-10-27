6118764deff015fe9be6b956f39d2a10d4a83429
dadiazpr <dadiazpr@dadiazprs-MacBook-Air.local>
2025-10-27 22:14:42 +0100

    Update DB v.5.0

--- bicifood_db_v4.0/bicifood_dbV4.0.sql
+++ bicifood_db_v4.0/bicifood_dbV4.0.sql
@@ -29,7 +29,7 @@ CREATE TABLE `categoria` (
   `nom_cat` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id_categoria`),
   UNIQUE KEY `nom_cat` (`nom_cat`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,14 +39,12 @@ CREATE TABLE `categoria` (
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
 INSERT INTO `categoria` VALUES 
-(1,'PLATS'),
-(2,'BEGUDES'),
-(3,'POSTRES'),
-(4,'CARNS'),
-(5,'PEIXOS'),
-(6,'AMANIDES'),
-(7,'ARRÒS I PASTA'),
-(8,'VINS');
+(1,'CARNS'),
+(2,'PEIXOS'),
+(3,'AMANIDES I VEGETALS'),
+(4,'ARRÒS I PASTA'),
+(5,'BEGUDES'),
+(6,'POSTRES');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -197,42 +195,38 @@ CREATE TABLE `producte` (
 LOCK TABLES `producte` WRITE;
 /*!40000 ALTER TABLE `producte` DISABLE KEYS */;
 INSERT INTO `producte` VALUES 
--- CARNS (categoria 4)
-(1,'Vedella amb arròs de l\'hort',13.95,'images/Vedella Arròs.png',50,4,'Suculenta carn de vedella, feta al forn de pedra, amb arròs Basmati, acompanyat amb espàrrecs i tomàquets.'),
-(2,'Vedella amb verdures',14.50,'images/Vedella Verdures.png',45,4,'Tendra vedella a la planxa servida amb verdures de temporada saltejades.'),
-(3,'Pollastre amb xampinyons',12.95,'images/Pollastre Xampi.png',60,4,'Pit de pollastre a la planxa amb xampinyons salvatger i salsa de fines herbes.'),
-(4,'Pollastre amb verdures',11.95,'images/Pollatre amb verdures.png',55,4,'Pollastre rostit acompanyat de verdures fresques de l\'hort.'),
+-- CARNS (categoria 1)
+(1,'Vedella amb arròs de l\'hort',13.95,'images/Vedella Arròs.png',50,1,'Suculenta carn de vedella, feta al forn de pedra, amb arròs Basmati, acompanyat amb espàrrecs i tomàquets.'),
+(2,'Vedella amb verdures',14.50,'images/Vedella Verdures.png',45,1,'Tendra vedella a la planxa servida amb verdures de temporada saltejades.'),
+(3,'Pollastre amb xampinyons',12.95,'images/Pollastre Xampi.png',60,1,'Pit de pollastre a la planxa amb xampinyons salvatger i salsa de fines herbes.'),
+(4,'Pollastre amb verdures',11.95,'images/Pollatre amb verdures.png',55,1,'Pollastre rostit acompanyat de verdures fresques de l\'hort.'),
 
--- PEIXOS (categoria 5)
-(5,'Lluç amb verdures',15.95,'images/Lluç Verdures.png',30,5,'Filet de lluç fresc a la planxa amb verdures mediterrànies.'),
-(6,'Peix a la sal',18.50,'images/Peix Sal.png',25,5,'Peix del dia cuit a la sal marina amb guarnició de verdures.'),
+-- PEIXOS (categoria 2)
+(5,'Lluç amb verdures',15.95,'images/Lluç Verdures.png',30,2,'Filet de lluç fresc a la planxa amb verdures mediterrànies.'),
+(6,'Peix a la sal',18.50,'images/Peix Sal.png',25,2,'Peix del dia cuit a la sal marina amb guarnició de verdures.'),
 
--- AMANIDES (categoria 6)
-(7,'Amanida Fresca',8.95,'images/Amanida Fresca.png',80,6,'Mix de fulles verdes fresques, tomàquet, cogombre i vinagreta de la casa.'),
-(8,'Amanida Gourmet',9.75,'images/Amanida Gourmet.png',75,6,'Amanida selecta amb fruits secs, formatge de cabra i vinagreta balsàmica.'),
+-- AMANIDES I VEGETALS (categoria 3)
+(7,'Amanida Fresca',8.95,'images/Amanida Fresca.png',80,3,'Mix de fulles verdes fresques, tomàquet, cogombre i vinagreta de la casa.'),
+(8,'Amanida Gourmet',9.75,'images/Amanida Gourmet.png',75,3,'Amanida selecta amb fruits secs, formatge de cabra i vinagreta balsàmica.'),
+(9,'Saltejat de verdures',9.50,'images/Saltejat de Verdures.png',90,3,'Verdures fresques saltejades al wok amb oli d\'oliva verge extra.'),
+(10,'Verdures rostides',8.95,'images/Verdures Rustides.png',85,3,'Verdures de temporada rostides al forn amb fines herbes.'),
 
--- ARRÒS I PASTA (categoria 7)
-(9,'Paella Valenciana',16.50,'images/Paella.png',40,7,'Autèntica paella valenciana amb pollastre, garrofó, bajoqueta i safrà.'),
-(10,'Espaguetis Carbonara',11.50,'images/Espaguetis carbonara.png',70,7,'Pasta fresca amb salsa carbonara tradicional italiana.'),
-(11,'Espaguetis al Pesto',10.95,'images/Espaguetis Pesto.png',65,7,'Espaguetis amb salsa pesto casolana i parmesà.'),
-(12,'Risotto',13.95,'images/Risotto.png',35,7,'Risotto cremós amb bolets i parmesà envellit.'),
+-- ARRÒS I PASTA (categoria 4)
+(11,'Paella Valenciana',16.50,'images/Paella.png',40,4,'Autèntica paella valenciana amb pollastre, garrofó, bajoqueta i safrà.'),
+(12,'Espaguetis Carbonara',11.50,'images/Espaguetis carbonara.png',70,4,'Pasta fresca amb salsa carbonara tradicional italiana.'),
+(13,'Espaguetis al Pesto',10.95,'images/Espaguetis Pesto.png',65,4,'Espaguetis amb salsa pesto casolana i parmesà.'),
+(14,'Risotto',13.95,'images/Risotto.png',35,4,'Risotto cremós amb bolets i parmesà envellit.'),
 
--- VERDURES (categoria 1 - PLATS)
-(13,'Saltejat de verdures',9.50,'images/Saltejat de Verdures.png',90,1,'Verdures fresques saltejades al wok amb oli d\'oliva verge extra.'),
-(14,'Verdures rostides',8.95,'images/Verdures Rustides.png',85,1,'Verdures de temporada rostides al forn amb fines herbes.'),
+-- BEGUDES (categoria 5) - Inclou refrescos i vins
+(15,'Coca-Cola',2.50,'images/Coca-cola.jpg',200,5,'Refrescant Coca-Cola en llauna de 33cl.'),
+(16,'Fanta Taronja',2.50,'images/Fanta.jpg',180,5,'Fanta taronja en llauna de 33cl.'),
+(17,'Aigua mineral',1.50,'images/Aigua.jpg',300,5,'Aigua mineral natural 50cl.'),
+(18,'Vi Torremadrina',15.95,'images/Vi Torremadrina.jpeg',25,5,'Vi negre D.O. amb notes afrutades i cos mitjà.'),
+(19,'Vi Viña Esmeralda',12.95,'images/Vi Viña Esmeralda.jpeg',30,5,'Vi blanc aromàtic amb tocs florals i frescs.'),
 
--- POSTRES (categoria 3)
-(15,'Coulant de xocolata',6.50,'images/Coulant.png',100,3,'Coulant de xocolata negra amb cor líquid i gelat de vainilla.'),
-(16,'Fruites del bosc',5.95,'images/Fruites del bosc.png',120,3,'Selecció de fruites del bosc fresques amb crema xantilly.'),
-
--- BEGUDES (categoria 2)
-(17,'Coca-Cola',2.50,'images/Coca-cola.jpg',200,2,'Refrescant Coca-Cola en llauna de 33cl.'),
-(18,'Fanta Taronja',2.50,'images/Fanta.jpg',180,2,'Fanta taronja en llauna de 33cl.'),
-(19,'Aigua mineral',1.50,'images/Aigua.jpg',300,2,'Aigua mineral natural 50cl.'),
-
--- VINS (categoria 8)
-(20,'Vi Torremadrina',15.95,'images/Vi Torremadrina.jpeg',25,8,'Vi negre D.O. amb notes afrutades i cos mitjà.'),
-(21,'Vi Viña Esmeralda',12.95,'images/Vi Viña Esmeralda.jpeg',30,8,'Vi blanc aromàtic amb tocs florals i frescs.');
+-- POSTRES (categoria 6)
+(20,'Coulant de xocolata',6.50,'images/Coulant.png',100,6,'Coulant de xocolata negra amb cor líquid i gelat de vainilla.'),
+(21,'Fruites del bosc',5.95,'images/Fruites del bosc.png',120,6,'Selecció de fruites del bosc fresques amb crema xantilly.');
 /*!40000 ALTER TABLE `producte` ENABLE KEYS */;
 UNLOCK TABLES;
 
